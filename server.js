d1d0de2c728258c97a8cb54166b14f68989b0314
Adarsh Vijayaraghavan <adarshvijayaraghavan@gmail.com>
2018-04-26 22:26:40 -0400

    front-end

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.post('/api/message', function(request, response){
+  console.log(request.body);
+  response.status(200);
+});
+
+var server = app.listen(5000, function(){
+  console.log("Server started in port", server.address().port);
+});
