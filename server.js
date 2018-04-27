0ded2899affce447926ae835391275111fb7d2c8
Adarsh Vijayaraghavan <adarshvijayaraghavan@gmail.com>
2018-04-26 23:32:02 -0400

    Mongo DB related changes. Will replace with mongoose next

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var mongo = require('mongodb').MongoClient;
 app.use(bodyParser.json());
+
+var database;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+})
+
 app.post('/api/message', function(request, response){
   console.log(request.body);
+  database.collection('messages').insertOne(request.body);
   response.status(200);
+});
+
+mongo.connect("mongodb://localhost:27017/test", function(err, client) {
+  if (!err) {
+     console.log("We are connected to Mongo DB");
+     database = client.db('test');
+  }
 });
 
 var server = app.listen(5000, function(){
